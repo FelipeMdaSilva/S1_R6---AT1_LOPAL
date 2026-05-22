caf8a750453d181b795a95bbb5eff3a71ab9cfc8
FelipeMdaSilva <fehfehsilva7@gmail.com>
2026-05-22 10:20:40 -0300

    Adicionando operações importando math

---
+++ index.py
@@ -0,0 +1,18 @@
+import math
+
+raio = 10
+base = 5
+expoente = 3
+
+#math.pi: mostra o valor do Pi
+perimetro = 2 * math.pi * raio
+
+#math.pow(): mostra o valor da base elevada ao expoente
+potencia = math.pow(base, expoente)
+
+#math.sqrt: mostra a raiz quadrada de um numero
+raiz_quadrada = math.sqrt(144)
+
+print(f"Perímetro: {perimetro:.2f}")
+print(f"Potência: {potencia}")
+print(f"Raiz Quadrada: {raiz_quadrada}")
